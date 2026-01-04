88a99e641eb62cae934216ad38129c45984e8379
alokgoldy <alokkrgoldy@gmail.com>
2026-01-05 00:34:52 +0530

    5 jan 2026

---
+++ Array/merge_sorted_array.cpp
@@ -0,0 +1,43 @@
+question link - https://leetcode.com/problems/merge-sorted-array/
+
+
+// js
+var merge = function(nums1, m, nums2, n) {
+    let i = m-1;
+    let j = n-1;
+    let k = m+n-1;
+
+    while(j>=0){
+
+        if (i>=0 && nums1[i] > nums2[j]){
+            nums1[k] = nums1[i];
+            i--;
+        } else{
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+};
+
+// c++
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(j>=0){
+
+            if (i>=0 && nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            } else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+}
