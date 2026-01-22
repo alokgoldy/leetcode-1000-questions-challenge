5eb781ef12c14c7d16d1143642878d902b8edef0
alokgoldy <alokkrgoldy@gmail.com>
2026-01-22 22:03:00 +0530

    22 jan 2026

---
+++ Array/max_consecutive_ones_III.cpp
@@ -0,0 +1,39 @@
+q link: https://leetcode.com/problems/max-consecutive-ones-iii/
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int res = 0;
+        int left = 0;
+        int sum = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+            while (sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let res = 0;
+    let left = 0;
+    let sum = 0;
+    for(let right = 0; right<nums.length; right++){
+        sum+=nums[right];
+        while(sum>k){
+            sum-=nums[left];
+            left++;
+        }
+        res = Math.max(res, right-left+1);
+    }
+    return res;
+};
