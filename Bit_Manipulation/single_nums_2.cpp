dab47127b8974727d118bb91dc57449fccc1909e
alokgoldy <alokkrgoldy@gmail.com>
2026-01-23 23:51:00 +0530

    23 jan 2026

--- Bit_Manipulation/single_nums_2.cpp
+++ Bit_Manipulation/single_nums_2.cpp
@@ -43,3 +43,22 @@ public:
         return result;
     }
 };
+
+
+// 🚀 Approach 2: Bitmask / FSM (most elegant)
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+
+        return ones;
+    }
+};
