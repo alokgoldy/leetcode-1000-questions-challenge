fbf113eeaec90b394fb8b60cb390dbcdc395e828
alokgoldy <alokkrgoldy@gmail.com>
2026-01-23 23:50:24 +0530

    23 jan 2026

--- Bit_Manipulation/single_nums_2.cpp
+++ Bit_Manipulation/single_nums_2.cpp
@@ -17,3 +17,29 @@ public:
         return -1;
     }
 };
+
+
+// bit manipulation
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bitCount = 0;
+
+            for (int num : nums) {
+                if ((num >> i) & 1) {
+                    bitCount++;
+                }
+            }
+
+            if (bitCount % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+
+        return result;
+    }
+};
