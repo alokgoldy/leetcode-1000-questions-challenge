4645bb7834915e76dd3acbfbbcdcebc01cc08b8e
alokgoldy <alokkrgoldy@gmail.com>
2026-03-06 10:10:29 +0530

    6 march 2026

--- Array/2_sum.cpp
+++ Array/2_sum.cpp
@@ -36,3 +36,23 @@ var twoSum = function(nums, target) {
 
     return res;
 };
+
+// revision 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int,int>mp;
+
+        for(int i=0; i< nums.size(); i++){
+            int num = nums[i];
+
+            if(mp.find(target - num) != mp.end()){
+                return {i, mp[target - num]};
+            }
+            mp[num] = i;
+        }
+        return {};
+    }
+};
