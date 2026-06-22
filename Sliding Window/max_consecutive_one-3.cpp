5fb488376304b43eed94593014cb39045c4cf086
alokgoldy <alokkrgoldy@gmail.com>
2026-06-22 23:00:31 +0530

    22 june 2026

--- Sliding Window/max_consecutive_one-3.cpp
+++ Sliding Window/max_consecutive_one-3.cpp
@@ -16,3 +16,22 @@ public:
         return nums.size() - left;
     }
 };
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0;
+
+        for(int right = 0; right < nums.size(); right++){
+            if(nums[right] == 0) k--;
+
+            if(k < 0){
+                if(nums[left] == 0){
+                    k++;
+                }
+                left++;
+            }
+        }
+        return nums.size() - left;
+    }
+};
