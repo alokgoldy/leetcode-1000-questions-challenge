72a94c3545f297d565f03371a315a418b5626f87
alokgoldy <alokkrgoldy@gmail.com>
2026-06-29 21:33:56 +0530

    29 june 2026

--- Sliding Window/max_consecutive_one-3.cpp
+++ Sliding Window/max_consecutive_one-3.cpp
@@ -35,3 +35,26 @@ public:
         return nums.size() - left;
     }
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let i = 0, j = 0;
+
+    while(j < nums.length){
+        if(nums[j++] === 0){
+            k--;
+        }
+
+        if(k<0){
+            if(nums[i++] === 0){
+                k++;
+            }
+        }
+    }
+    return j - i;
+};
